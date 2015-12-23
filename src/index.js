598590b1fc715479afb9bfaada537dad313990f0
James McAllister <youbiteme@gmail.com>
2015-12-23 12:46:32 +0000

    added jsx

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-// Var HelloWorld = React.createClass({
-//   render: function() {
-//     return (
-//       <p>
-//         Hello, <input type='text' placeholder='Your name here' />!
-//         It is {this.props.date.toTimeString()}
-//       </p>
-//     );
-//   },
-// });
-//
-// SetInterval(function() {
-//   ReactDOM.render(
-//     <HelloWorld date={new Date()} />,
-//     document.getElementById('example')
-//   );
-// }, 500);
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <p>
+        Hello, <input type='text' placeholder='Your name here' />!
+        It is {this.props.date.toTimeString()}
+      </p>
+    );
+  },
+});
+
+SetInterval(function() {
+  ReactDOM.render(
+    <HelloWorld date={new Date()} />,
+    document.getElementById('example')
+  );
+}, 500);
