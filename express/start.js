62c92952a6f6145240f82d814bf15ad94937a11d
James McAllister <youbiteme@gmail.com>
2015-12-24 01:59:50 +0000

    added cssnext

--- express/start.js
+++ express/start.js
@@ -2,10 +2,10 @@ var express = require('express');
 var server = express();
 server.use('/app',express.static('dist/app'))
 server.use(express.static('src/html'))
-server.listen(3000,"localhost", function(err) {
-  if(err) {
+server.listen(3000,'localhost', function(err) {
+  if (err) {
     console.log(err);
     return;
   }
-  console.log("started")
+  console.log('started')
 })
--- src/js/router.js
+++ src/js/router.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Router, Route, Link } from 'react-router';
 import { render } from 'react-dom';
-var mainElement = document.querySelector("#main");
-if(!mainElement ){
-  mainElement = document.createElement("div");
-  mainElement.setAttribute("id","main");
-  var bodyElement = document.getElementsByTagName("body").item(0);
+var mainElement = document.querySelector('#main');
+if (!mainElement) {
+  mainElement = document.createElement('div');
+  mainElement.setAttribute('id','main');
+  var bodyElement = document.getElementsByTagName('body').item(0);
   bodyElement.appendChild(mainElement);
 }
 var HelloWorld = React.createClass({
-  render: function () {
+  render: function() {
     return (
       <p>
         Hello, <input type='text' placeholder='Your name here'/>!
@@ -19,7 +19,7 @@ var HelloWorld = React.createClass({
 });
 render  (
 <Router>
-  <Route path="/" component={HelloWorld}>
+  <Route path='/' component={HelloWorld}>
     <Route path='hello' handeler={HelloWorld} />
   </Route>
 </Router>,mainElement
